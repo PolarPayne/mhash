bd2155b495b52fd329f9b46529e3a7a51f359ab1
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-02-06 19:07:21 +0200

    Fixed indentation in test.c

--- test.c
+++ test.c
@@ -8,8 +8,8 @@ int mu_tests_run = 0;
 
 static char* test_crc_derp()
 {
-	FILE* fp = fopen("tests/derp.png", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/derp.png", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of derp.png wasn't 0xac340e73", 0xac340e73 == hash);
@@ -18,8 +18,8 @@ static char* test_crc_derp()
 
 static char* test_crc_fox()
 {
-	FILE* fp = fopen("tests/fox.txt", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/fox.txt", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of fox.txt wasn't 0x519025e9", 0x519025e9 == hash);
@@ -28,8 +28,8 @@ static char* test_crc_fox()
 
 static char* test_crc_index()
 {
-	FILE* fp = fopen("tests/index.html", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/index.html", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of index.html wasn't 0xc5813ba0", 0xc5813ba0 == hash);
@@ -38,8 +38,8 @@ static char* test_crc_index()
 
 static char* test_crc_mom()
 {
-	FILE* fp = fopen("tests/mom.txt", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/mom.txt", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of mom.txt wasn't 0x124f3381", 0x124f3381 == hash);
@@ -48,8 +48,8 @@ static char* test_crc_mom()
 
 static char* test_crc_random0()
 {
-	FILE* fp = fopen("tests/random0", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/random0", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of random0 wasn't 0x124f3381", 0xf60e41de == hash);
@@ -58,8 +58,8 @@ static char* test_crc_random0()
 
 static char* test_crc_random1()
 {
-	FILE* fp = fopen("tests/random1", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/random1", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of random1 wasn't 0xf112b0cb", 0xf112b0cb == hash);
@@ -68,8 +68,8 @@ static char* test_crc_random1()
 
 static char* test_crc_random2()
 {
-	FILE* fp = fopen("tests/random2", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/random2", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of random2 wasn't 0xc06d8a53", 0xc06d8a53 == hash);
@@ -78,8 +78,8 @@ static char* test_crc_random2()
 
 static char* test_crc_random3()
 {
-	FILE* fp = fopen("tests/random3", "rb");
-	uint64_t hash = mhash_crc_file(fp);
+    FILE* fp = fopen("tests/random3", "rb");
+    uint64_t hash = mhash_crc_file(fp);
     fclose(fp);
 
     mu_assert("CRC32 of random3 wasn't 0x41b38c37", 0x41b38c37 == hash);
@@ -102,7 +102,7 @@ static char* all_tests()
 
 int main()
 {
-	char* result = all_tests();
+    char* result = all_tests();
     if (result != 0)
         printf("%s\n", result);
     else
