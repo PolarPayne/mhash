82230b79ab3347b5c177507309431390f2f1c7f2
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-02-06 23:25:09 +0200

    Clarified a comment

--- mhash_parity.c
+++ mhash_parity.c
@@ -1,7 +1,8 @@
 #include "mhash_parity.h"
 
 /*
- * Precalculated table with even parities.
+ * Precalculated table with even parities
+ * of all 8 bit unsigned integers.
  */
 static uint8_t parity_table[256] = {
     0, 1, 1, 0, 1, 0, 0, 1,
@@ -43,10 +44,8 @@ uint8_t mhash_parity_buf(uint8_t parity, uint8_t* data, size_t len, uint8_t type
 	assert(len > 0);
 
     uint8_t p = data[0];
-    //printf("%" PRIx8 "\n", p);
 	for (size_t i = 1; i < len; i++) {
 		p ^= data[i];
-        //printf("%" PRIx8 "\n", p);
 	}
 
 	return parity ^ (parity_table[p] ^ type);
