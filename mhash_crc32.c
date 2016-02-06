55698cc69be5e690bc4a3bd4d3cb5bd568703fce
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-02-06 20:19:29 +0200

    Fixed crc functio to not take a pointer

--- mhash_crc32.c
+++ mhash_crc32.c
@@ -1,10 +1,10 @@
 #include "mhash_crc32.h"
 
-uint32_t static mhash_crc(uint32_t crc, uint8_t* data)
+uint32_t static mhash_crc(uint32_t crc, uint8_t data)
 {
 	uint32_t rem = ~crc;
 	for (size_t bit = 0; bit < 8; bit++) {
-        if ((rem & 0x1) != ((*data>>bit) & 0x1)) {
+        if ((rem & 0x1) != ((data>>bit) & 0x1)) {
             rem = (rem >> 0x1) ^ (0xEDB88320);
         } else {
             rem = rem >> 0x1;
@@ -18,7 +18,7 @@ uint32_t mhash_crc_buf(uint32_t crc, uint8_t* data, size_t len)
 	assert(len > 0);
 
 	for (size_t i = 0; i < len; i++) {
-		crc = mhash_crc(crc, &data[i]);
+		crc = mhash_crc(crc, data[i]);
 	}
 	return crc;
 }
