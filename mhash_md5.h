351b47ba4c87f00264e7733496e57a5297c4e0f4
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-02-15 21:44:00 +0200

    Removed SHA1 and MD5 code

--- mhash_md5.h
+++ mhash_md5.h
@@ -1,14 +1,10 @@
 #ifndef MHASH_MD5_H
 #define MHASH_MD5_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <inttypes.h>
+#include <stdio.h>
+#include <assert.h>
 
-uint8_t* mhash_md5_buf(uint8_t*, uint32_t);
 uint8_t* mhash_md5_file(FILE*);
-
-char* mhash_md5_str(uint8_t*);
 
 #endif //MHASH_MD5_H
