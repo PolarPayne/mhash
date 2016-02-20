34e94b5f802eb6236b85d9ad1d1b2ee6dc96104b
Maximilian Remming <maximilian.remming@cs.helsinki.fi>
2016-02-20 14:47:22 +0200

    Librarizing didn't work

--- mhash_crc32.h
+++ mhash_crc32.h
@@ -1,15 +1,13 @@
-#ifndef MHASH_CRC32_H
-#define MHASH_CRC32_H
+#ifndef CRC32_H
+#define CRC32_H
 
 #include <inttypes.h>
 #include <stdio.h>
 #include <assert.h>
 
-#include "mhash_errors.h"
-
 #define MHASH_READBUFFER_SIZE 512
 
-int mhash_crc32_buf(uint32_t*, uint8_t*, size_t);
-int mhash_crc32_file(FILE*, uint32_t*);
+uint32_t mhash_crc32_buf(uint32_t, uint8_t*, size_t);
+uint32_t mhash_crc32_file(FILE*);
 
-#endif // MHASH_CRC32_H
+#endif // CRC32_H
